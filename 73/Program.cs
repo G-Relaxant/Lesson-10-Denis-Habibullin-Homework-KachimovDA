47254ac18a0c8d9722d8565316c4abae2f517310
geekbrains <info@geekbrains.ru>
2022-07-17 13:32:16 +0400

    edit

--- 73/Program.cs
+++ 73/Program.cs
@@ -1,35 +1,44 @@
-﻿Console.Write("Введите число N: ");
+﻿Console.Write("Введите число от 1 до N: ");
 long number = long.Parse(Console.ReadLine());
 long[] array = new long[number];
 FillArray(array);
 Console.WriteLine($"Количество чисел для группировки: {number}");
-long arrayValuesZeroingCounter = 0;
+TestPrintArray(array, number);
+Console.WriteLine();
+long arrayValuesZeroingCounter = 1;
 int groupNumber = 1;
 Console.WriteLine($"Группа {groupNumber}: {array[0]}");
 array[0] = 0;
 
-while(arrayValuesZeroingCounter < number)
+while(arrayValuesZeroingCounter < number)   //   1
 {
+    Console.WriteLine($"TEST 1");
     long[] group = new long[number];
-    long lastCellGroupIndex = 1;
+    long lastCellGroupIndex = 2;
     long FirstIndexGroupValueSearchRelay = 0;
-    for(long FirstIndexGroupValueSearch = 0; array[FirstIndexGroupValueSearch] == 0 || FirstIndexGroupValueSearch < number; FirstIndexGroupValueSearch++)
+    for(long ind = 0; array[FirstIndexGroupValueSearchRelay] == 0 && ind < number; ind++)  //  2
     {
-        FirstIndexGroupValueSearchRelay = FirstIndexGroupValueSearch;
+        Console.WriteLine($"TEST 2");
+        FirstIndexGroupValueSearchRelay = ind + 1;
     }
-    FirstIndexGroupValueSearchRelay++;
-    group[0] = FirstIndexGroupValueSearchRelay;
-    for(long i = FirstIndexGroupValueSearchRelay + 1; i < number; i++)
+    //FirstIndexGroupValueSearchRelay++;
+    group[0] = array[FirstIndexGroupValueSearchRelay];
+    for(long i = 0; i < number; i++)  //  3
     {
+        Console.WriteLine($"TEST 3");
         long groupIndex = 0;
         if(array[i] != 0)
         {
-            while(groupIndex < lastCellGroupIndex)
+            Console.WriteLine($"TEST 3 1");
+            while(groupIndex < lastCellGroupIndex)  //  4
             {
+                Console.WriteLine($"TEST 4");
                 if(array[i] % group[groupIndex] != 0)
                 {
+                    Console.WriteLine($"TEST 4 1 YES");
                     if(groupIndex == lastCellGroupIndex - 1)
                     {
+                        Console.WriteLine($"TEST 4 2");
                         group[groupIndex] = array[i];
                         lastCellGroupIndex++;
                         array[i] = 0;
@@ -38,6 +47,7 @@ while(arrayValuesZeroingCounter < number)
                 }
                 else
                 {
+                    Console.WriteLine($"TEST 4 1 NO");
                     break;
                 }
                 groupIndex++;
@@ -47,6 +57,7 @@ while(arrayValuesZeroingCounter < number)
     groupNumber++;
     Console.Write($"Группа {groupNumber}: ");
     PrintGroup(group, lastCellGroupIndex);
+    Console.WriteLine($"Test LCGI {lastCellGroupIndex}, AVZC {arrayValuesZeroingCounter}");
     Console.WriteLine();
 }
 
@@ -62,6 +73,19 @@ void FillArray(long[] a)
 }
 
 void PrintGroup(long[] a, long b)
+{
+    Console.Write("[");
+    for(long i = 0; i < b; i++)
+    {
+        Console.Write(a[i]);
+        if(i < b - 1)
+            Console.Write(", ");
+        else
+            Console.WriteLine("]");
+    }
+}
+
+void TestPrintArray(long[] a, long b)
 {
     Console.Write("[");
     for(long i = 0; i < b; i++)
